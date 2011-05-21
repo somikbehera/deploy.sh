7c3ce0e12d587809e876063f2a2aba086d70a081
Jesse Andrews <anotherjesse@gmail.com>
2011-05-20 22:32:36 -0700

    lockfile was missing and eventlet needs to be our patched version

--- nova.sh
+++ nova.sh
@@ -80,10 +80,10 @@ if [ "$CMD" == "install" ]; then
     sudo apt-get install -y python-twisted python-mox python-ipy python-paste
     sudo apt-get install -y python-migrate python-gflags python-greenlet
     sudo apt-get install -y python-libvirt python-libxml2 python-routes
-    sudo apt-get install -y python-netaddr python-pastedeploy python-eventlet
+    sudo apt-get install -y python-netaddr python-pastedeploy python-eventlet=0.9.12-0ubuntu2
     sudo apt-get install -y python-novaclient python-glance python-cheetah
     sudo apt-get install -y python-carrot python-tempita python-sqlalchemy
-    sudo apt-get install -y python-suds
+    sudo apt-get install -y python-suds python-lockfile
 
 
     if [ "$USE_IPV6" == 1 ]; then
