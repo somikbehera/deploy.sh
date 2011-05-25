3ae74a84d762733e6dc8374e988e4b4374f23685
Jesse Andrews <anotherjesse@gmail.com>
2011-05-24 17:09:31 -0700

    run nova.sh in detach_mode

--- auto.sh
+++ auto.sh
@@ -4,11 +4,12 @@ set -o errexit
 curl -O https://github.com/cloudbuilders/deploy.sh/raw/master/nova.sh
 chmod 755 nova.sh
 
-export USE_GIT=1         # checkout source using github mirror
-export ENABLE_VOLUMES=0  # disable volumes
-export ENABLE_DASH=1     # install & configure dashboard
-export ENABLE_KEYSTONE=1 # install & configure keystone (unified auth)
+export USE_GIT=1
+export ENABLE_VOLUMES=0
+export ENABLE_DASH=1
+export ENABLE_GLANCE=1
+export ENABLE_KEYSTONE=1
 
 ./nova.sh branch
 ./nova.sh install
-./nova.sh run # FIXME - there is a way to run this in detached mode
+./nova.sh run_detached
