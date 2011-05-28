be470662c898a256751591ace66afb4255e73315
Jesse Andrews <anotherjesse@gmail.com>
2011-05-28 02:18:16 -0700

    wait for api to launch before registery

--- nova.sh
+++ nova.sh
@@ -276,6 +276,7 @@ if [ "$CMD" == "run" ] || [ "$CMD" == "run_detached" ]; then
         rm -rf /var/lib/glance/images/*
         rm -f $GLANCE_DIR/glance.sqlite
         screen_it glance-api "cd $GLANCE_DIR; bin/glance-api --config-file=etc/glance-api.conf"
+        sleep 2
         screen_it glance-registry "cd $GLANCE_DIR; bin/glance-registry --config-file=etc/glance-registry.conf"
 
         # wait 10 seconds to let glance launch
