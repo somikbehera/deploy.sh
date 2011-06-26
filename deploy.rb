d855cda6ba044472811a18ffdcfd3f5266a52fc3
Jesse Andrews <anotherjesse@gmail.com>
2011-06-26 16:54:59 -0700

    allow setting REAL=1 cap puppet:kick

--- deploy.rb
+++ deploy.rb
@@ -11,9 +11,15 @@ set :user, 'demo'
 set :ssh_options, { :forward_agent => true }
 set :use_sudo, true
 
-role :cpu,  "cpu.rcb.me"
-role :infra, "dash.rcb.me"
-role :puppet, "puppet.rcb.me"
+if ENV['REAL']
+  role :cpu, "10.4.99.2", "10.4.99.3", "10.4.99.4", "10.4.99.5", "10.4.99.6"
+  role :infra, "10.4.99.1"
+  role :puppet, "10.4.99.1"
+else
+  role :cpu,  "cpu.rcb.me"
+  role :infra, "dash.rcb.me"
+  role :puppet, "puppet.rcb.me"
+end
 
 namespace :puppet do
   desc "pull latests code from github to puppetmaster"
@@ -23,6 +29,7 @@ namespace :puppet do
   
   desc "run puppet on hosts"
   task :kick, :roles => [:cpu, :infra] do
+    sudo "apt-get update"
     sudo "puppetd -t"
   end
   
