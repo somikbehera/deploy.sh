2f6f0fcb5de1dc9df4c2fd61bead3412ddf73d40
Anthony Young <sleepsonthefloor@gmail.com>
2011-07-19 20:32:34 -0700

    add the ability to run alternative versions of nova.sh

--- auto.sh
+++ auto.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-curl -O https://raw.github.com/cloudbuilders/deploy.sh/master/nova.sh
-chmod 755 nova.sh
+NOVASCRIPT=${NOVASCRIPT:nova.sh}
+curl -O https://raw.github.com/cloudbuilders/deploy.sh/master/$NOVASCRIPT
+chmod 755 $NOVASCRIPT
 
 export USE_GIT=1
 export ENABLE_VOLUMES=0
@@ -12,12 +13,12 @@ export ENABLE_KEYSTONE=1
 export ENABLE_APACHE=1
 export NET_MAN=FlatDHCPManager
 
-./nova.sh branch
-./nova.sh install
+./$NOVASCRIPT branch
+./$NOVASCRIPT install
 
 # HACK: cloud servers requires older libvirt
 if [ ! -z "$CLOUDSERVER" ] ; then
     apt-get install -y --force-yes libvirt0=0.8.3-1ubuntu18 libvirt-bin=0.8.3-1ubuntu18 python-libvirt=0.8.3-1ubuntu18
 fi
 
-./nova.sh run_detached
+./$NOVASCRIPT run_detached
