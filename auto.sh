8c44c4cb436940004b1ebbf6337f102453269a7b
Chmouel Boudjnah <chmouel@chmouel.com>
2011-08-30 20:50:13 -0500

    release of libvirt package has been bumped to 19.1

--- auto.sh
+++ auto.sh
@@ -20,7 +20,7 @@ export NET_MAN=FlatDHCPManager
 
 # HACK: cloud servers requires older libvirt
 if [ ! -z "$CLOUDSERVER" ] ; then
-    apt-get install -y --force-yes libvirt0=0.8.3-1ubuntu18 libvirt-bin=0.8.3-1ubuntu18 python-libvirt=0.8.3-1ubuntu18
+    apt-get install -y --force-yes libvirt0=0.8.3-1ubuntu19.1 libvirt-bin=0.8.3-1ubuntu19.1 python-libvirt=0.8.3-1ubuntu19.1
 fi
 
 ./$NOVASCRIPT run_detached
